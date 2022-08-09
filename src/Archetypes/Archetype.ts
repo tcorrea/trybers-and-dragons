df0bb4c582cfe0c971612ec44c19b2ed7d17f6eb
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-08-09 18:20:50 -0300

    feat: requisito 04 - Archetype class criado classe Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(
+    private _name: string,
+  ) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  abstract get energyType(): EnergyType;
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get special(): number {
+    return this._special;
+  }
+
+  public get cost(): number {
+    return this._cost;
+  }
+
+  public static createdArchetypeInstances() {
+    throw Error('Not implemented');
+  }
+}
